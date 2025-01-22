3fd4115354982b693fa96437234e344ea878c27b
Sandip <jaiswarsandeep00@gmail.com>
2025-01-22 14:44:49 +0530

    update

--- deserializer_application.py
+++ deserializer_application.py
@@ -1,17 +1,17 @@
+
 import requests
 import json
-
-URL ='http://127.0.0.1:8000/marvel'
+URL ='http://127.0.0.1:8000/newmarvel/'
 
 data={
-    'f_name':'Abhishek',
-    'heroic_name':'More'
+    'f_name':'Natasha Romanoff',
+    'heroic_name':'black widow'
 }
 
 json_data = json.dumps(data)
-print(json_data)
-res = requests.post(url=URL,data=json_data)
 
-data= res.json()
+response = requests.post(url=URL, data=json_data)
+
+data=response.json()
 
 print(data)
