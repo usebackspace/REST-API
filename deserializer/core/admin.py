90607e438c9d8ce88c872f91ae76244af104b43e
Sandip <jaiswarsandeep00@gmail.com>
2025-01-22 14:40:48 +0530

    delete

--- deserializer/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . models import Marvel
-# Register your models here.
-
-@admin.register(Marvel)
-class MarvelAdmin(admin.ModelAdmin):
-    list_display=['id','f_name','heroic_name']
--- deserializer/core/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Marvel
-
-class MarvelSerializer(serializers.Serializer):
-    f_name = serializers.CharField(max_length=50)
-    heroic_name = serializers.CharField(max_length=50)
-
-    def create(self, validated_data):
-        return Marvel.objects.create(**validated_data)
